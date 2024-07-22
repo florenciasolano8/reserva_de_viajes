5e83602139aa5fbb4dd7734a06a30c595df9128a
florencia solano <solanoflorencia16@gmail.com>
2024-07-22 11:50:15 -0300

    acualizo

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
 let cartPacks = loadCartPacks()
 
-function loadCartPacks(){
+function loadCartPacks() {
     const storedCartsPacks = localStorage.getItem("cartPacks")
     return storedCartsPacks ? JSON.parse(storedCartsPacks) : []
 }
 
-function saveCartPacks(){
+function saveCartPacks() {
     localStorage.setItem("cartPacks", JSON.stringify(cartPacks))
 }
 
 
 let combos = document.getElementById("packs")
-fetch("../db/data.JSON")
+fetch("./db/packs.JSON")
     .then(response => response.json())
-    .then(packs =>{
-       packs.forEach(item =>{
-        const card = document.createElement("div")
-        card.classList.add("card")
-        card.innerHTML = `
+    .then(packs => {
+        packs.forEach(item => {
+            const card = document.createElement("div")
+            card.classList.add("card")
+            card.innerHTML = `
                          <div class="card-body">
                           <img  class="card-img-top" src="${item.image}" alt="naturaleza">
                           <h2 class= "card-title">${item.title}</h2>
@@ -29,47 +29,47 @@ fetch("../db/data.JSON")
                           <button class="packReserve btn " id="${item.id}">Reservar </button>
                           </div>      
                         </div>`
-                        combos.appendChild(card)    
+            combos.appendChild(card)
+        })
+        addReservationButton(packs)
     })
-    addReservationButton(packs)
-})
 
-.catch(error=>{
-    console.error('error',error)
-})
+    .catch(error => {
+        console.error('error', error)
+    })
 
 
-function addReservationButton(packs){
+function addReservationButton(packs) {
     addButton = document.querySelectorAll(".packReserve")
-    addButton.forEach(button=>{
-        button.onclick = (e)=>{
+    addButton.forEach(button => {
+        button.onclick = (e) => {
             const itemId = e.currentTarget.id
             const selectedPack = packs.find(item => item.id == itemId)
-            
+
             const quantityInput = document.querySelector(`#quantity-${itemId}`)
             const quantity = parseInt(quantityInput.value)
 
-            
-            if(isNaN(quantity) || quantity < 1){
+
+            if (isNaN(quantity) || quantity < 1) {
                 Swal.fire({
                     icon: "error",
                     title: "Oops...",
                     text: "Ingrese la cantidad de reserva que desea!",
-                  })
+                })
                 return
             }
 
 
-            let itemExist = cartPacks.find(item => item.id == selectedPack.id)  
-            if(itemExist){
+            let itemExist = cartPacks.find(item => item.id == selectedPack.id)
+            if (itemExist) {
                 itemExist.quantity = quantity
-           }
-            else{
-            selectedPack.quantity = quantity
-            cartPacks.push(selectedPack)
+            }
+            else {
+                selectedPack.quantity = quantity
+                cartPacks.push(selectedPack)
 
-           }
-           
+            }
+
             saveCartPacks()
 
             Toastify({
@@ -78,18 +78,18 @@ function addReservationButton(packs){
                 destination: "https://github.com/apvarun/toastify-js",
                 newWindow: true,
                 close: true,
-                gravity: "top", 
-                position: "right", 
-                stopOnFocus: true, 
+                gravity: "top",
+                position: "right",
+                stopOnFocus: true,
                 style: {
-                  background: "linear-gradient(to right, #3a47b4, #a25be8)",
+                    background: "linear-gradient(to right, #3a47b4, #a25be8)",
                 },
-                onClick: function(){} 
-              }).showToast()
-       
-       
+                onClick: function () { }
+            }).showToast()
+
+
         }
     })
 
-}  
+}
 
--- js/mireserva.js
+++ js/mireserva.js
@@ -4,138 +4,138 @@ reservationStorage = reservationStorage ? JSON.parse(reservationStorage) : []
 let cart = document.getElementById("reservation")
 
 function reservationCart(cartItems) {
-    let totalToPay = 0
-    const cardsContainer = document.createElement("div")
-    cardsContainer.classList.add("cards-container")
-    cartItems.forEach((item, index) => {
-        const card = document.createElement("div")
-        const total = item.price * item.quantity
-        totalToPay += total
-        card.classList.add("card")
-        card.innerHTML = `
+  let totalToPay = 0
+  const cardsContainer = document.createElement("div")
+  cardsContainer.classList.add("cards-container")
+  cartItems.forEach((item, index) => {
+    const card = document.createElement("div")
+    const total = item.price * item.quantity
+    totalToPay += total
+    card.classList.add("card")
+    card.innerHTML = `
                         <div class="card-body">
                         <h2 class= "card-title">${item.title}</h2>
                         <p class="card-text">Cantidad de vuelos:  ${item.quantity}</p>
                         <p class="card-text">Total: $${total}</p>
                         <button class="delete-btn" onclick="deleteCard(${index})"> X </button>
-                        </div>` 
-        cardsContainer.appendChild(card)
-    })
-    cart.appendChild(cardsContainer)
+                        </div>`
+    cardsContainer.appendChild(card)
+  })
+  cart.appendChild(cardsContainer)
 
 
-    const totalFinal = document.createElement("div")
-    totalFinal.classList.add("totalfinal")
-    totalFinal.innerHTML = `
+  const totalFinal = document.createElement("div")
+  totalFinal.classList.add("totalfinal")
+  totalFinal.innerHTML = `
                          <button class="button-clear btn " onclick="deleteAllCards()">Vaciar carrito</button>
                          <h3 class="subtitulopagar">TOTAL A PAGAR: $${totalToPay}</h3>`
-    cart.appendChild(totalFinal)
+  cart.appendChild(totalFinal)
 }
 
 
 function deleteAllCards() {
-    Swal.fire({
-        title: "Estas seguro?",
-        icon: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#3085d6",
-        cancelButtonColor: "#d33",
-        cancelButtonText: "Cancelar",
-        confirmButtonText: "Si vaciar!"
-      }).then((result) => {
-        if (result.isConfirmed) {
-            localStorage.removeItem("cartPacks")
-            document.getElementById("reservation").innerHTML = ""
-            document.getElementById("message").innerHTML=""
-            reservationCart([])
+  Swal.fire({
+    title: "Estas seguro?",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    cancelButtonText: "Cancelar",
+    confirmButtonText: "Si vaciar!"
+  }).then((result) => {
+    if (result.isConfirmed) {
+      localStorage.removeItem("cartPacks")
+      document.getElementById("reservation").innerHTML = ""
+      document.getElementById("message").innerHTML = ""
+      reservationCart([])
 
-          Swal.fire({
-            title: "Listo!",
-            icon: "success"
-          })
-        }
+      Swal.fire({
+        title: "Listo!",
+        icon: "success"
       })
+    }
+  })
 
 }
 
 function deleteCard(index) {
-    Swal.fire({
-        title: "Estas seguro?",
-        icon: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#3085d6",
-        cancelButtonColor: "#d33",
-        cancelButtonText: "Cancelar",
-        confirmButtonText: "Si borrar!"
-      }).then((result) => {
-        if (result.isConfirmed) {
-            reservationStorage.splice(index, 1)
-            localStorage.setItem("cartPacks", JSON.stringify(reservationStorage))
-            document.getElementById("reservation").innerHTML = ""
-            reservationCart(reservationStorage)
+  Swal.fire({
+    title: "Estas seguro?",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    cancelButtonText: "Cancelar",
+    confirmButtonText: "Si borrar!"
+  }).then((result) => {
+    if (result.isConfirmed) {
+      reservationStorage.splice(index, 1)
+      localStorage.setItem("cartPacks", JSON.stringify(reservationStorage))
+      document.getElementById("reservation").innerHTML = ""
+      reservationCart(reservationStorage)
 
-          Swal.fire({
-            title: "Borrado!",
-            icon: "success"
-          })
-        }
+      Swal.fire({
+        title: "Borrado!",
+        icon: "success"
       })
+    }
+  })
 
 }
 
-function clearForm(){
-    Swal.fire({
-        title: "Estas seguro?",
-        icon: "warning",
-        showCancelButton: true,
-        confirmButtonColor: "#3085d6",
-        cancelButtonColor: "#d33",
-        cancelButtonText: "Cancelar",
-        confirmButtonText: "Si borrar!"
-      }).then((result) => {
-        if (result.isConfirmed) {
+function clearForm() {
+  Swal.fire({
+    title: "Estas seguro?",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#3085d6",
+    cancelButtonColor: "#d33",
+    cancelButtonText: "Cancelar",
+    confirmButtonText: "Si borrar!"
+  }).then((result) => {
+    if (result.isConfirmed) {
 
-        const name = document.getElementById("name").value
-        const lastname = document.getElementById("lastname").value
-        const date = document.getElementById("datePicker").value
-    
-        let reservations = localStorage.getItem("reservations")    
-        reservations = reservations ? JSON.parse(reservations) : []    
-    
-        const reservationIndex = reservations.findIndex(reservation =>
-            reservation.name === name &&
-            reservation.lastName === lastname &&
-            reservation.date === date
-        )
-    
-        if(reservationIndex !== -1){
-            reservations.splice(reservationIndex, 1)
-            localStorage.setItem("reservations", JSON.stringify(reservations))
-        }
-        document.getElementById("name").value = ""
-        document.getElementById("lastname").value = ""
-        document.getElementById("datePicker").value = ""
-        document.getElementById("message").innerHTML=""
+      const name = document.getElementById("name").value
+      const lastname = document.getElementById("lastname").value
+      const date = document.getElementById("datePicker").value
+
+      let reservations = localStorage.getItem("reservations")
+      reservations = reservations ? JSON.parse(reservations) : []
+
+      const reservationIndex = reservations.findIndex(reservation =>
+        reservation.name === name &&
+        reservation.lastName === lastname &&
+        reservation.date === date
+      )
+
+      if (reservationIndex !== -1) {
+        reservations.splice(reservationIndex, 1)
+        localStorage.setItem("reservations", JSON.stringify(reservations))
+      }
+      document.getElementById("name").value = ""
+      document.getElementById("lastname").value = ""
+      document.getElementById("datePicker").value = ""
+      document.getElementById("message").innerHTML = ""
 
 
 
-          Swal.fire({
-            title: "Reserva eliminada!",
-            icon: "success"
-          })
-        }
+      Swal.fire({
+        title: "Reserva eliminada!",
+        icon: "success"
       })
+    }
+  })
 
-    
+
 }
 
 reservationCart(reservationStorage)
 
-const {DateTime} = luxon
+const { DateTime } = luxon
 const today = DateTime.now().toFormat('yyyy-MM-dd')
 const inputDate = document.getElementById('datePicker')
-if(inputDate){
-  inputDate.setAttribute('min',today)
+if (inputDate) {
+  inputDate.setAttribute('min', today)
 }
 
 let inputName = document.getElementById("name")
@@ -145,101 +145,101 @@ let message = document.getElementById("message")
 
 
 if (inputDate && searchBtn) {
-    searchBtn.onclick = () => {
-        const nameValue = inputName.value
-        const lastNameValue = inputLastName.value
-        const dateValue = inputDate.value
+  searchBtn.onclick = () => {
+    const nameValue = inputName.value
+    const lastNameValue = inputLastName.value
+    const dateValue = inputDate.value
 
-        const namePattern = /^[a-zA-Z]+$/
+    const namePattern = /^[a-zA-Z]+$/
 
-      if(!nameValue || !lastNameValue ||!dateValue){
+    if (!nameValue || !lastNameValue || !dateValue) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Por favor completa todos los campos",
+      })
+      return
+    }
+
+    if (!nameValue.match(namePattern) || !lastNameValue.match(namePattern)) {
+
+      Toastify({
+        text: "Ingrese solo letras en los campos nombre y apellido",
+        duration: 4000,
+        destination: "https://github.com/apvarun/toastify-js",
+        newWindow: true,
+        close: true,
+        gravity: "top",
+        position: "center",
+        stopOnFocus: true,
+        style: {
+          background: "linear-gradient(to right, #3a47b4, #a25be8)",
+        },
+        onClick: function () { }
+      }).showToast()
+      return
+    }
+
+    let reservations = localStorage.getItem("reservations")
+    reservations = reservations ? JSON.parse(reservations) : []
+
+    const existingReservation = reservations.find(
+      (reserva) => reserva.date === dateValue
+    )
+    if (existingReservation) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Ya hay una reserva para esa fecha ",
+      })
+      message.innerHTML = `<p>Ya hay una reserva para el ${dateValue} . Ingrese otra fecha</p>`
+    }
+    else {
+
+      const reserva = {
+        name: nameValue,
+        lastName: lastNameValue,
+        date: dateValue,
+      }
+
+      reservations.push(reserva)
+      localStorage.setItem("reservations", JSON.stringify(reservations))
+      Swal.fire({
+        position: "center",
+        icon: "success",
+        title: "Reservado",
+        showConfirmButton: false,
+        timer: 1500
+      })
+      message.innerHTML = `<p>${nameValue} ${lastNameValue} tiene una reserva para el ${dateValue} </p>`
+
+    }
+  }
+
+}
+
+//Paso mi api de feriados al calendario de reservas
+
+fetch("../db/holidays.JSON")
+  .then(response => response.json())
+  .then(data => {
+    const holidays = data.map(item => item.fecha)
+
+    inputDate.addEventListener('change', (event) => {
+      const selectedDate = event.target.value
+      if (holidays.includes(selectedDate)) {
         Swal.fire({
           icon: "error",
-          title: "Oops...",
-          text: "Por favor completa todos los campos",
+          title: "Feriado"
+        }).then(() => {
+          inputDate.value = ""
         })
-        return
       }
-
-      if(!nameValue.match(namePattern) || !lastNameValue.match(namePattern)){
-
-          Toastify({
-            text: "Ingrese solo letras en los campos nombre y apellido",
-            duration: 4000,
-            destination: "https://github.com/apvarun/toastify-js",
-            newWindow: true,
-            close: true,
-            gravity: "top", 
-            position: "center", 
-            stopOnFocus: true, 
-            style: {
-              background: "linear-gradient(to right, #3a47b4, #a25be8)",
-            },
-            onClick: function(){} 
-          }).showToast()
-        return
-      }
-
-        let reservations = localStorage.getItem("reservations")
-        reservations = reservations ? JSON.parse(reservations) : []
-
-        const existingReservation = reservations.find(
-        (reserva) => reserva.date === dateValue
-            )
-            if (existingReservation) {
-                Swal.fire({
-                    icon: "error",
-                    title: "Oops...",
-                    text: "Ya hay una reserva para esa fecha ",
-                  })
-                message.innerHTML = `<p>Ya hay una reserva para el ${dateValue} . Ingrese otra fecha</p>`
-            } 
-            else{
-
-                const reserva = {
-                    name: nameValue,
-                    lastName: lastNameValue,
-                    date: dateValue,
-                }
-
-                reservations.push(reserva)
-                localStorage.setItem("reservations", JSON.stringify(reservations))
-                Swal.fire({
-                    position: "center",
-                    icon: "success",
-                    title: "Reservado",
-                    showConfirmButton: false,
-                    timer: 1500
-                  })
-                message.innerHTML = `<p>${nameValue} ${lastNameValue} tiene una reserva para el ${dateValue} </p>`
-          
-              }
-        }
-    
-    }
- 
-    //Paso mi api de feriados al calendario de reservas
-
-    fetch("../db/holidays.JSON")
-    .then(response => response.json())
-    .then(data =>{
-    const holidays= data.map(item=>item.fecha);
-    
-      inputDate.addEventListener('change', (event) =>{
-        const selectedDate = event.target.value
-        if(holidays.includes(selectedDate)){
-          Swal.fire({
-            icon:"error",
-            title:"Feriado"
-          }).then(()=>{
-            inputDate.value = ""
-          })
-        }
-      })
     })
-  
+  })
 
-    const now =DateTime.now()
-    const footer = document.getElementsByName('footer')
-    message.innerHTML = now.toLocaleString(DateTime.DATETIME_SHORT)
-    message.innerHTML = now.toFormat('dd/MM/yyyy HH:mm')
+
+const now = DateTime.now()
+const footer = document.getElementsByName('footer')
+message.innerHTML = now.toLocaleString(DateTime.DATETIME_SHORT)
+message.innerHTML = now.toFormat('dd/MM/yyyy HH:mm')
